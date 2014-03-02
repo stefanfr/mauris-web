bb49121596d5e74ce3ad935985d2d726f11f3504
Marlin Cremers <m.cremers@mms-projects.net>
2014-03-02 15:55:29 +0100

    Fixed the post to comment relationship

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -19,7 +19,7 @@ class Post extends AppModel {
     public $hasMany = array(
         'Comments' => array(
             'className' => 'Comment',
-            'foreignKey' => 'reply_to'
+            'foreignKey' => 'post_id'
         )
     );
     
