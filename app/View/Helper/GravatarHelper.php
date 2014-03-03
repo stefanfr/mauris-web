64aa044fd174e691bd1be21c5f67ef908c8a9c6c
Marlin Cremers <m.cremers@mms-projects.net>
2014-03-03 20:36:45 +0100

    Added a urlencode to the Gravatar menu helper to make the urls valid

--- app/View/Helper/GravatarHelper.php
+++ app/View/Helper/GravatarHelper.php
@@ -7,7 +7,7 @@ class GravatarHelper extends AppHelper {
     public function gravatar($email, array $gravatarOptions = array(), array $options = array()) {
         $url = 'http://www.gravatar.com/avatar/';
         $url .= md5(strtolower(trim($email)));
-        $url .= '?' . http_build_query($gravatarOptions);
+        $url .= '?' . urlencode(http_build_query($gravatarOptions));
         return $this->Html->image($url, array_merge($options, array('alt' => 'CakePHP')));
     }
 
