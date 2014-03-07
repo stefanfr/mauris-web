b4c0c46c6f190dc8825f7cb83dc3bf3a9f02b0da
Marlin Cremers <m.cremers@mms-projects.net>
2014-03-07 16:54:04 +0100

    Added some more pages to the sitemap

--- app/Controller/SitemapsController.php
+++ app/Controller/SitemapsController.php
@@ -3,12 +3,71 @@
 class SitemapsController extends AppController { 
 
     public $name = 'Sitemaps'; 
-    public $uses = array('Department', 'ScheduleEntry'); 
+    public $uses = array('Department', 'ScheduleEntry', 'Post', 'Page');
     public $helpers = array('Time'); 
     public $components = array('RequestHandler'); 
 
     public function index() {     
         $this->set('departments', $this->Department->find('all'));
+        $this->set('posts', $this->Post->find(
+            'all',
+            array(
+                'recursive' => -1,
+                'conditions' => array(
+                    'and' => array(
+                        array(
+                            'or' => array(
+                                array(
+                                    'and' => array(
+                                        'school_id IS NULL',
+                                        'department_id IS NULL'
+                                    )
+                                ),
+                                array(
+                                    'and' => array(
+                                        'school_id' => $this->School->id,
+                                        'department_id IS NULL'
+                                    )
+                                ),
+                                array(
+                                    'and' => array(
+                                        'school_id' => $this->School->id,
+                                        'department_id' => $this->Department->id
+                                    )
+                                )
+                            )
+                        ),
+                        'published' => 1,
+                    )
+                ),
+                'fields' => array(
+                    'id', 'title'
+                )
+            )
+        ));
+        $this->set('pages', $this->Page->find(
+            'all',
+            array(
+                'recursive' => -1,
+                'conditions' => array(
+                    'and' => array(
+                        array(
+                            'or' => array(
+                                array(
+                                    'and' => array(
+                                        'school_id' => $this->School->id,
+                                        'department_id' => $this->Department->id
+                                    )
+                                )
+                            )
+                        ),
+                    )
+                ),
+                'fields' => array(
+                    'id', 'title'
+                )
+            )
+        ));
         $scheduleEntries = $this->ScheduleEntry->find(
             'all',
             array(
