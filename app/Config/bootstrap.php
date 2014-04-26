2e5b99509d35669820aac270b08c894e5ebdbc5c
Marlin Cremers <m.cremers@cvo-technologies.com>
2014-04-26 21:45:59 +0200

    Enable the schedule plugin only when not on the main website
    Change-Id: Ic1617749648de00e12f2944b8f0b0a059a5b6ee1

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -80,9 +80,10 @@ CakePlugin::load('Manage');
 CakePlugin::load('Intermediary', array('routes' => true));
 CakePlugin::load('Billboard', array('routes' => true));
 CakePlugin::load('OvInfo');
-CakePlugin::load('Schedule', array('routes' => true));
 if ($_SERVER['SERVER_NAME'] == 'mauris.systems') {
     CakePlugin::load('Website', array('routes' => true));
+} else {
+	CakePlugin::load('Schedule', array('routes' => true));
 }
 
 /**
