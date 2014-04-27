6ed50c5e9a204e370a0c92791400a3009b1bbb17
Marlin Cremers <m.cremers@cvo-technologies.com>
2014-04-27 14:40:45 +0200

    Created a inflector rule to correctly singularize caches into cache
    Change-Id: I0f18e2b56831326efb9239638140becf68d7ee33

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -60,6 +60,10 @@ Cache::config('default', array('engine' => 'File'));
  *
  */
 
+Inflector::rules('singular', array('irregular' => array(
+	'caches' => 'cache'
+)));
+
 /**
  * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
  * Uncomment one of the lines below, as you need. Make sure you read the documentation on CakePlugin to use more
