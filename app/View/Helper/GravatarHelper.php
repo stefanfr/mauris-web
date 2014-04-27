72e512b4dceaaa8f794780d12bd260d6af8b61d4
Marlin Cremers <m.cremers@cvo-technologies.com>
2014-04-27 17:21:26 +0200

    Added Gravatar hash support to the Gravatar helper
    Change-Id: I0f23550ee64097a3e4d53d31f49629fe28469a3c

--- app/View/Helper/GravatarHelper.php
+++ app/View/Helper/GravatarHelper.php
@@ -4,9 +4,13 @@ class GravatarHelper extends AppHelper {
 
     public $helpers = array('Html');
 
-    public function gravatar($email, array $gravatarOptions = array(), array $options = array()) {
-        $url = 'http://www.gravatar.com/avatar/';
-        $url .= md5(strtolower(trim($email)));
+    public function gravatar($hash, array $gravatarOptions = array(), array $options = array()) {
+        if (strstr('@', $hash)) {
+			$hash = md5(strtolower(trim($hash)));
+		}
+
+		$url = 'http://www.gravatar.com/avatar/';
+        $url .= $hash;
         $url .= '?' . urlencode(http_build_query($gravatarOptions));
         return $this->Html->image($url, array_merge($options, array('alt' => 'CakePHP')));
     }
