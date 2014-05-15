813313b904fd26759dc0521fce3cff3dc6fd14b6
Marlin Cremers <m.cremers@cvo-technologies.com>
2014-05-15 20:11:43 +0200

    Add /contributions as a route to the contributions controller index

--- app/Plugin/Website/Config/routes.php
+++ app/Plugin/Website/Config/routes.php
@@ -5,6 +5,7 @@ Router::promote();
 
 Router::connect('/contribute', array('plugin' => 'website', 'controller' => 'contributions', 'action' => 'contribute'));
 Router::connect('/contribute/*', array('plugin' => 'website', 'controller' => 'contributions', 'action' => 'info'));
+Router::connect('/contributions/*', array('plugin' => 'website', 'controller' => 'contributions'));
 
 Router::connect('/organizations/:action/*', array('plugin' => null, 'controller' => 'organizations', 'website' => true));
 Router::connect('/organizations/*', array('plugin' => null, 'controller' => 'organizations', 'action' => 'index', 'website' => true));
