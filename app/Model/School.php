e7b05f2bdfb378125a7cbc3ee26cec46808f730b
Marlin Cremers <m.cremers@cvo-technologies.com>
2014-05-15 15:20:15 +0200

    Add validation to the school model

--- app/Model/School.php
+++ app/Model/School.php
@@ -3,7 +3,22 @@
 class School extends AppModel {
 
 	public $displayField = 'name';
-        
+
+	public $validate = array(
+		'name' => array(
+			array(
+				'rule'    => 'notEmpty',
+				'message' => 'Enter a name for the organization'
+			)
+		),
+		'logo' => array(
+			array(
+				'rule'       => 'url',
+				'allowEmpty' => true
+			)
+		)
+	);
+
         public $hasMany = array(
             'HasDepartments' => array(
                 'className' => 'Department',
