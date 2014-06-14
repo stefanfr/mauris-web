e3b7ca022c359dd18307df176374926bad5dbb17
Marlin Cremers <m.cremers@cvo-technologies.com>
2014-06-14 17:03:58 +0200

    Add method to Gravatar helper to get a avatar URL

--- app/View/Helper/GravatarHelper.php
+++ app/View/Helper/GravatarHelper.php
@@ -7,14 +7,20 @@ class GravatarHelper extends AppHelper {
     public $helpers = array('Html');
 
     public function gravatar($hash, array $gravatarOptions = array(), array $options = array()) {
-        if (strlen($hash) !== 32) {
+
+        return $this->Html->image($this->avatarUrl($hash, $gravatarOptions), array_merge($options, array('alt' => 'CakePHP')));
+    }
+
+	public function avatarUrl($hash, array $gravatarOptions = array()) {
+		if (strlen($hash) !== 32) {
 			$hash = md5(strtolower(trim($hash)));
 		}
 
 		$url = 'http://www.gravatar.com/avatar/';
-        $url .= $hash;
-        $url .= '?' . http_build_query($gravatarOptions);
-        return $this->Html->image($url, array_merge($options, array('alt' => 'CakePHP')));
-    }
+		$url .= $hash;
+		$url .= '?' . http_build_query($gravatarOptions);
+
+		return $url;
+	}
 
 }
